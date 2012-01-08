d4aa48360c0e2510b5fa7beaca36a3d7ea2417f4
Grigoriy Dzhanelidze <ohaistarlight@gmail.com>
2012-01-08 21:53:36 +0400

    Updated gravity and blocks level.

--- package/levels.py
+++ package/levels.py
@@ -40,8 +40,8 @@ class Level:
 
 	def jump(self):
 		if self.eng.player.jumpheight > 0:
-			self.eng.player.rect.y -= self.eng.g + 3
-			self.eng.player.jumpheight -= self.eng.g + 3
+			self.eng.player.rect.y -= self.eng.g + 1
+			self.eng.player.jumpheight -= self.eng.g + 1
 		else:
 			self.eng.player.jumpheight = 0
 
@@ -63,7 +63,7 @@ class Level1(Level):
 		self.eng.set_background(entity.Entity('sun.png',0,0))
 		self.eng.add_entity(entity.Text("Press arrows or WASD to move",42,"m46.TTF",(60,265),(255,255,255)))
 		self.eng.add_entity(entity.Text("Exit ->",34,"m46.TTF",(500,400),(255,255,255)))
-		self.eng.add_entity(entity.Entity('hero.png',10,396,"player",40))
+		self.eng.add_entity(entity.Entity('hero.png',10,396,"player",45))
 		self.eng.add_entity(entity.Entity('pic2.png',-20,440,scale=(680,80)))
 		self.eng.add_entity(entity.Entity('exit.png',605,396,"exit"))
 		while self.going and not self.completed:
@@ -86,7 +86,7 @@ class Level2(Level):
 		self.completed = False
 		self.eng.set_background(entity.Entity('sun.png',0,0))
 		self.eng.add_entity(entity.Text("Press up or space to jump",42,"m46.TTF",(105,265),(255,255,255)))
-		self.eng.add_entity(entity.Entity('hero.png',10,396,"player",40))
+		self.eng.add_entity(entity.Entity('hero.png',10,396,"player",45))
 		self.eng.add_entity(entity.Entity('pic2.png',-20,440,scale=(680,80)))
 		self.eng.add_entity(entity.Entity('pic2.png',500,420,scale=(50,20)))
 		self.eng.add_entity(entity.Entity('pic2.png',550,400,scale=(50,40)))
@@ -112,7 +112,7 @@ class Level3(Level):
 		self.completed = False
 		self.eng.set_background(entity.Entity('sun.png',0,0))
 		self.eng.add_entity(entity.Text("OMG IT'S MOVING!",42,"m46.TTF",(160,265),(255,255,255)))
-		self.eng.add_entity(entity.Entity('hero.png',10,396,"player",40))
+		self.eng.add_entity(entity.Entity('hero.png',10,396,"player",45))
 		self.eng.add_entity(entity.Entity('pic2.png',0,440,scale=(100,80)))
 		self.eng.add_entity(entity.Lift('platform.png',270,440,110,455,440,440,-3))
 		self.eng.add_entity(entity.Entity('pic2.png',551,440,scale=(100,80)))
@@ -157,7 +157,7 @@ class Level5(Level):
 		self.eng.add_entity(entity.Text("Where am I?",36,"m46.TTF",(180,-36),(255,255,255)))
 		self.eng.add_entity(entity.Text("Where is everyone?",36,"m46.TTF",(500,93),(255,255,255)))
 		self.eng.add_entity(entity.Text("It was a day a minute ago...",36,"m46.TTF",(480,-20),(255,255,255)))
-		self.eng.add_entity(entity.Entity('hero.png',10,396,"player",40))
+		self.eng.add_entity(entity.Entity('hero.png',10,396,"player",45))
 		self.eng.add_entity(entity.Entity('pic2.png',0,440,scale=(100,80)))
 		self.eng.add_entity(entity.Lift('platform.png',110,400,110,110,110,445,dy=-3))
 		self.eng.add_entity(entity.Entity('pic2.png',195,115,scale=(200,60)))
@@ -187,7 +187,7 @@ class Level6(Level):
 		self.eng.set_background(entity.Entity('moon.png',0,0))
 		self.eng.add_entity(entity.Text("Hello?",36,"m46.TTF",(50,320),(255,255,255)))
 		self.eng.add_entity(entity.Text("Anybody?...",36,"m46.TTF",(480,-40),(255,255,255)))
-		self.eng.add_entity(entity.Entity('hero.png',10,396,"player",40))
+		self.eng.add_entity(entity.Entity('hero.png',10,396,"player",45))
 		self.eng.add_entity(entity.Entity('pic2.png',0,440,scale=(150,80)))
 		self.eng.add_entity(entity.Lift('platform.png',480,440,155,480,0,0,-3))
 		self.eng.add_entity(entity.Lift('platform.png',570,440,0,0,115,440,dy=-3))
@@ -214,26 +214,25 @@ class Level7(Level):
 		self.going = True
 		self.completed = False
 		self.eng.set_background(entity.Entity('sun.png',0,0))
-		self.eng.add_entity(entity.Text("This world is strange",36,"m46.TTF",(50,220),(255,255,255)))
-		self.eng.add_entity(entity.Entity('hero.png',50,396,"player",40))
+		self.eng.add_entity(entity.Text("This world is strange",36,"m46.TTF",(-20,220),(255,255,255)))
+		self.eng.add_entity(entity.Text("Don't hurry",36,"m46.TTF",(650,220),(255,255,255)))
+		self.eng.add_entity(entity.Entity('hero.png',50,396,"player",45))
 		self.eng.add_entity(entity.Entity('pic2.png',0,440,scale=(130,30)))
 		self.eng.add_entity(entity.Entity('pic2.png',100,410,scale=(30,30)))
 		self.eng.add_entity(entity.Entity('pic2.png',150,380,scale=(30,30)))
 		self.eng.add_entity(entity.Entity('pic2.png',230,360,scale=(30,30)))
 		self.eng.add_entity(entity.Entity('pic2.png',310,340,scale=(30,30)))
 		self.eng.add_entity(entity.Entity('pic2.png',410,330,scale=(30,30)))
-		self.eng.add_entity(entity.Entity('pic2.png',410,330,scale=(30,30)))
-		self.eng.add_entity(entity.Entity('pic2.png',550,350,scale=(30,30)))
-		self.eng.add_entity(entity.Entity('pic2.png',650,330,scale=(30,30)))
-		self.eng.add_entity(entity.Entity('pic2.png',760,330,scale=(30,30)))
-		self.eng.add_entity(entity.Entity('pic2.png',880,340,scale=(30,30)))
-		self.eng.add_entity(entity.Entity('pic2.png',1020,350,scale=(30,30)))
-		self.eng.add_entity(entity.Entity('pic2.png',1130,335,scale=(30,30)))
-		self.eng.add_entity(entity.Entity('pic2.png',1230,315,scale=(30,30)))
-		self.eng.add_entity(entity.Entity('pic2.png',1330,300,scale=(30,30)))
-		self.eng.add_entity(entity.Entity('pic2.png',1430,285,scale=(30,30)))
-		self.eng.add_entity(entity.Entity('pic2.png',1430,285,scale=(130,30)))
-		self.eng.add_entity(entity.Entity('exit.png',1525,241,"exit"))
+		self.eng.add_entity(entity.Entity('pic2.png',535,350,scale=(30,30)))
+		self.eng.add_entity(entity.Entity('pic2.png',650,340,scale=(30,30)))
+		self.eng.add_entity(entity.Entity('pic2.png',750,330,scale=(30,30)))
+		self.eng.add_entity(entity.Entity('pic2.png',850,340,scale=(30,30)))
+		self.eng.add_entity(entity.Entity('pic2.png',970,350,scale=(30,30)))
+		self.eng.add_entity(entity.Entity('pic2.png',1070,335,scale=(30,30)))
+		self.eng.add_entity(entity.Entity('pic2.png',1170,315,scale=(30,30)))
+		self.eng.add_entity(entity.Entity('pic2.png',1270,300,scale=(30,30)))
+		self.eng.add_entity(entity.Entity('pic2.png',1370,285,scale=(130,30)))
+		self.eng.add_entity(entity.Entity('exit.png',1460,241,"exit"))
 		while self.going and not self.completed:
 			self.needs_respawn()
 			self.eng.update_camera()
@@ -257,7 +256,7 @@ class Level8(Level):
 		self.eng.add_entity(entity.Text("Far far away",36,"m46.TTF",(350,290),(255,255,255)))
 		self.eng.add_entity(entity.Text("Is it...?",36,"m46.TTF",(670,265),(255,255,255)))
 		self.eng.add_entity(entity.Text("I need to get there as soon as I can!",36,"m46.TTF",(900,265),(255,255,255)))
-		self.eng.add_entity(entity.Entity('hero.png',50,396,"player",40))
+		self.eng.add_entity(entity.Entity('hero.png',50,396,"player",45))
 		self.eng.add_entity(entity.Entity('pic2.png',0,440,scale=(300,80)))
 		self.eng.add_entity(entity.Entity('pic2.png',300,410,scale=(300,110)))
 		self.eng.add_entity(entity.Entity('pic2.png',600,380,scale=(300,140)))
@@ -284,7 +283,7 @@ class Level9(Level):
 		self.going = True
 		self.completed = False
 		self.eng.set_background(entity.Entity('sun.png',0,0))
-		self.eng.add_entity(entity.Entity('hero.png',30,195,"player",40))
+		self.eng.add_entity(entity.Entity('hero.png',30,195,"player",45))
 		self.eng.add_entity(entity.Entity('pic2.png',0,240,scale=(150,80)))
 		self.eng.add_entity(entity.Entity('pic2.png',200,260,scale=(40,40)))
 		self.eng.add_entity(entity.Entity('pic2.png',290,280,scale=(40,40)))
@@ -316,7 +315,7 @@ class Level10(Level):
 		self.eng.set_background(entity.Entity('moon.png',0,0))
 		self.eng.add_entity(entity.Text("What the hell?",36,"m46.TTF",(490,-120),(255,255,255)))
 		self.eng.add_entity(entity.Text("phew",36,"m46.TTF",(520,420),(255,255,255)))
-		self.eng.add_entity(entity.Entity('hero.png',30,195,"player",40))
+		self.eng.add_entity(entity.Entity('hero.png',30,195,"player",45))
 		self.eng.add_entity(entity.Entity('pic2.png',0,240,scale=(150,80)))
 		self.eng.add_entity(entity.Entity('pic2.png',200,220,scale=(40,40)))
 		self.eng.add_entity(entity.Entity('pic2.png',290,200,scale=(40,40)))
@@ -348,7 +347,7 @@ class LevelPrelast(Level):
 		self.completed = False
 		self.eng.set_background(entity.Entity('moon.png',0,0))
 		self.eng.add_entity(entity.Text("I have found you!",36,"m46.TTF",(150,265),(255,255,255)))
-		self.eng.add_entity(entity.Entity('hero.png',10,396,"player",40))
+		self.eng.add_entity(entity.Entity('hero.png',10,396,"player",45))
 		self.eng.add_entity(entity.Entity('pic2.png',-20,440,scale=(680,80)))
 		self.eng.add_entity(entity.Entity('black.png',595,360,"exit",scale=(10,80)))
 		self.eng.add_entity(entity.Entity('fhero.png',605,396))
@@ -375,7 +374,7 @@ class LevelLast(Level):
 		self.eng.add_entity(entity.Text("Wait. It was night minute ago.",36,"m46.TTF",(450,200),(255,255,255)))
 		self.eng.add_entity(entity.Text("Where's she?",36,"m46.TTF",(1050,200),(255,255,255)))
 		self.eng.add_entity(entity.Text("Why this happens to me?",36,"m46.TTF",(1500,200),(255,255,255)))
-		self.eng.add_entity(entity.Entity('hero.png',10,396,"player",40))
+		self.eng.add_entity(entity.Entity('hero.png',10,396,"player",45))
 		self.eng.add_entity(entity.Entity('pic2.png',0,440,scale=(100,80)))
 		self.eng.add_entity(entity.Lift('platform.png',270,440,110,1705,0,0,-3))
 		self.eng.add_entity(entity.Entity('pic2.png',1800,440,scale=(100,80)))
@@ -401,7 +400,7 @@ class LevelSrslyLast(Level):
 		self.eng.set_background(entity.Entity('sun.png',0,0))
 		self.eng.add_entity(entity.Text("I will never find her again",36,"m46.TTF",(10,200),(255,255,255)))
 		self.eng.add_entity(entity.Text("Why can't I be happy for a while? :(",36,"m46.TTF",(275,-150),(255,255,255)))
-		self.eng.add_entity(entity.Entity('hero.png',10,396,"player",40))
+		self.eng.add_entity(entity.Entity('hero.png',10,396,"player",45))
 		self.eng.add_entity(entity.Entity('pic2.png',0,440,scale=(150,80)))
 		self.eng.add_entity(entity.Lift('platform.png',155,360,110,110,110,445,dy=-3))
 		self.eng.add_entity(entity.Entity('pic2.png',240,115,scale=(50,60)))

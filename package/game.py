47340e1bb6767272fb805499e29641275cc327b9
Grigoriy Dzhanelidze <ohaistarlight@gmail.com>
2012-04-18 19:53:54 +0400

    broken physics ftw

--- package/game.py
+++ package/game.py
@@ -1,4 +1,6 @@
-import engine,levels,pyclbr
+import engine
+import pyclbr
+import levels
 from levels import *
 
 def start():
@@ -9,16 +11,14 @@ def start():
 	eng.delay(1500)
 	eng.clear()
 	eng.set_background(entity.Entity("black.png", 0, 0))
-	eng.add_entity(entity.Text("G", 72, "m46.TTF", (230,230), (255,0,0)))
-	eng.add_entity(entity.Text("L", 72, "m46.TTF", (270,230), (0,255,0)))
-	eng.add_entity(entity.Text("O", 72, "m46.TTF", (290,230), (0,0,255)))
-	eng.add_entity(entity.Text("W", 72, "m46.TTF", (340,230), (255,255,255)))
+	eng.add_entity(entity.Text("G", 72, "m46.TTF", (230, 230), (255, 0, 0)))
+	eng.add_entity(entity.Text("L", 72, "m46.TTF", (270, 230), (0, 255, 0)))
+	eng.add_entity(entity.Text("O", 72, "m46.TTF", (290, 230), (0, 0, 255)))
+	eng.add_entity(entity.Text("W", 72, "m46.TTF", (340, 230), (255, 255, 255)))
 	eng.render()
 	eng.delay(2000)
 	eng.clear()
-	lvls = []
-	for level in [x for x in pyclbr.readmodule("package.levels").keys() if x.startswith("Level") and len(x) > 5]:
-		lvls.append(getattr(levels, level)(eng))
+	lvls = [getattr(levels, level)(eng) for level in pyclbr.readmodule("package.levels").keys() if level.startswith("Level") and len(level) > 5]
 	for level in lvls:
 		while not level.completed:
 			level.run()
